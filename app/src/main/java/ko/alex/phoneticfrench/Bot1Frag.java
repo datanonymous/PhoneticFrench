12d0cbfde5249c2a54add77f1a78e9651e5154d7
Alex Ko <ko.e.alexander@gmail.com>
2019-03-16 12:11:57 -0400

    successfully added tabs to bottom navigation

--- app/src/main/java/ko/alex/phoneticfrench/Bot1Frag.java
+++ app/src/main/java/ko/alex/phoneticfrench/Bot1Frag.java
@@ -1,12 +1,26 @@
 package ko.alex.phoneticfrench;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bot1Frag extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    FragmentActivity myContext;
 
     public Bot1Frag() {
         // Required empty public constructor
@@ -18,7 +32,57 @@ public class Bot1Frag extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.bot1frag, container, false);
 
+        viewPager = view.findViewById(R.id.ViewPager);
+        setupViewPager(viewPager);
+
+        tabLayout = view.findViewById(R.id.TabLayoutBot1Frag);
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+
+
+        //https://stackoverflow.com/questions/20237531/how-can-i-access-getsupportfragmentmanager-in-a-fragment
+        ViewPagerAdapter adapter = new ViewPagerAdapter(myContext.getSupportFragmentManager());
+        adapter.addFragment(new TabFrag11(), "TabFrag11");
+        adapter.addFragment(new TabFrag12(), "TabFrag12");
+        adapter.addFragment(new TabFrag13(), "TabFrag13");
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    //https://stackoverflow.com/questions/20237531/how-can-i-access-getsupportfragmentmanager-in-a-fragment
+    @Override
+    public void onAttach(Activity activity) {
+        myContext=(FragmentActivity) activity;
+        super.onAttach(activity);
+    }
+
+
 
 }
